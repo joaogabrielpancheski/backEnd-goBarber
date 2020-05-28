d5a190965fb2a663b0ad9b6b42bb500092f31485
João Gabriel Pancheski <joao.ti@grupointegrado.br>
2020-05-27 23:27:46 -0300

    Implements handlebars

--- src/shared/container/providers/index.ts
+++ src/shared/container/providers/index.ts
@@ -6,17 +6,20 @@ import DiskStorageProvider from '@shared/container/providers/StorageProvider/imp
 import IMailProvider from '@shared/container/providers/MailProvider/models/IMailProvider';
 import EtherealMailProvider from '@shared/container/providers/MailProvider/implementations/EtherealMailProvider';
 
+import IMailTemaplateProvider from '@shared/container/providers/MailTemplateProvider/models/IMailTemplateProvider';
+import HandlebarsMailTemplateProvider from '@shared/container/providers/MailTemplateProvider/implementations/HandlebarsMailTemplateProvider';
+
 container.registerSingleton<IStorageProvider>(
   'StorageProvider',
   DiskStorageProvider,
 );
 
-// container.registerSingleton<IMailProvider>(
-//   'MailProvider',
-//   EtherealMailProvider,
-// );
-
 container.registerInstance<IMailProvider>(
   'MailProvider',
   new EtherealMailProvider(),
+);
+
+container.registerSingleton<IMailTemaplateProvider>(
+  'MailTemplateProvider',
+  HandlebarsMailTemplateProvider,
 );
