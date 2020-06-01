02e3260d53869f2746a0d9bcc28bc310a686cb4e
João Gabriel Pancheski <joao.ti@grupointegrado.br>
2020-06-01 02:17:36 -0300

    Implements rate limiter

---
+++ src/shared/infra/http/middlewares/rateLimiter.ts
@@ -0,0 +1,34 @@
+import { Request, Response, NextFunction } from 'express';
+import redis from 'redis';
+import { RateLimiterRedis } from 'rate-limiter-flexible';
+
+import AppError from '@shared/errors/AppError';
+
+const { REDIS_HOST, REDIS_PORT, REDIS_PASS } = process.env;
+
+const redisClient = redis.createClient({
+  host: REDIS_HOST,
+  port: Number(REDIS_PORT),
+  password: REDIS_PASS || undefined,
+});
+
+const limiter = new RateLimiterRedis({
+  storeClient: redisClient,
+  keyPrefix: 'ratelimit',
+  points: 5,
+  duration: 1,
+});
+
+export default async function rateLimiter(
+  req: Request,
+  res: Response,
+  next: NextFunction,
+): Promise<void> {
+  try {
+    await limiter.consume(req.ip);
+
+    return next();
+  } catch (err) {
+    throw new AppError('Too many requests', 429);
+  }
+}
